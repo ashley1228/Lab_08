d700698a180a71b3fe99438253ced7c9f8e877e4
ashley1228 <ashley_rigano@uri.edu>
2022-03-31 16:18:00 -0400

    added print helloworld

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,8 @@
-userName = input("Hello! Please enter your facebook username! ")
-userPassword = input("Hello! Please enter your facebook password! ")
-if(userName.lower() == "ashley" and userPassword == "hello")
-print("Welcome to facebook!")
-else
-print("Wrong credentials, try again!")
+print ("Hello World")
+
+#userName = input("Hello! Please enter your facebook username! ")
+#userPassword = input("Hello! Please enter your facebook password! ")
+#if(userName.lower() == "ashley" and userPassword == "hello")
+#print("Welcome to facebook!")
+#else
+#print("Wrong credentials, try again!")
